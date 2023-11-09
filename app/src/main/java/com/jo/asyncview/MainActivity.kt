ea2eacbf0f43905065340a5b60359cf5f99fbc19
Administrator <akak4456@naver.com>
2023-11-09 14:13:40 +0900

    VerySlowSyncView 만듬

--- app/src/main/java/com/jo/asyncview/MainActivity.kt
+++ app/src/main/java/com/jo/asyncview/MainActivity.kt
@@ -2,10 +2,25 @@ package com.jo.asyncview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val handler: Handler = Handler(Looper.getMainLooper())
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        updateText()
+    }
+
+    private fun updateText() {
+        count++
+        findViewById<TextView>(R.id.tv_test).text = count.toString()
+        handler.postDelayed({
+            updateText()
+        }, 15L)
     }
 }
---
+++ app/src/main/java/com/jo/asyncview/VerySlowSyncView.kt
@@ -0,0 +1,24 @@
+package com.jo.asyncview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class VerySlowSyncView(context: Context, attrs :AttributeSet? = null) : View(context ,attrs) {
+
+    private var sum: Long = 0L
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.run {
+            for(i in 0 until 1000000000) {
+                sum += i.toLong()
+            }
+            drawRect(0f,0f,100f,100f, Paint().apply {
+                color = Color.RED
+            })
+        }
+    }
+}
